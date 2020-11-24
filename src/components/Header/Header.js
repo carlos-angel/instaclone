dab58e8f4466f87ef5389f17e15f8a8aa071ae2b
Charly_Angel <c.a.a.a.503@gmail.com>
2020-11-24 17:11:20 -0600

    feat: componente header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Container, Grid, GridColumn, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import RightHeader from "./RightHeader";
+import Logo from "../../assets/png/instaclone.png";
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <Container>
+        <Grid>
+          <GridColumn width={3} className="header__logo">
+            <Link to="/">
+              <Image src={Logo} alt="Instaclone" />
+            </Link>
+          </GridColumn>
+          <GridColumn width={10}>
+            <p>Buscar</p>
+          </GridColumn>
+          <GridColumn width={3}>
+            <RightHeader />
+          </GridColumn>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
