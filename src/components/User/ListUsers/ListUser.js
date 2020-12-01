12d977ca69e1b8ffd130f4c894d1241f4b6bc16c
Charly_Angel <c.a.a.a.503@gmail.com>
2020-12-01 07:12:05 -0600

    feat: lista de seguidores

---
+++ src/components/User/ListUsers/ListUser.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Image } from "semantic-ui-react";
+import { useHistory } from "react-router-dom";
+import { size, map } from "lodash";
+import avatarNotFound from "../../../assets/png/avatar.png";
+import "./ListUser.scss";
+
+export default function ListUser({ users, setShowModal }) {
+  const history = useHistory();
+
+  const goToUser = (username) => {
+    setShowModal(false);
+    history.push(`/${username}`);
+  };
+
+  return (
+    <div className="list-users">
+      {size(users) === 0 ? (
+        <p className="list-users__not-users">No se han encontrado usuarios</p>
+      ) : (
+        map(users, (user, index) => (
+          <div
+            key={index}
+            className="list-users__user"
+            onClick={() => goToUser(user.username)}
+          >
+            <Image src={user.avatar || avatarNotFound} avatar />
+            <div>
+              <p>{user.name}</p>
+              <p>{user.username}</p>
+            </div>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
--- src/components/User/Profile/Followers/Followers.js
+++ src/components/User/Profile/Followers/Followers.js
@@ -1,10 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_FOLLOWERS } from "../../../../gql/follow";
 import { size } from "lodash";
+import ModalBasic from "../../../Modal/ModalBasic";
+import ListUsers from "../../ListUsers";
 import "./Followers.scss";
 
 export default function Follow({ username }) {
+  const [showModal, setShowModal] = useState(false);
+  const [titleModal, setTitleModal] = useState("");
+  const [childrenModal, setChildrenModal] = useState(null);
+
   const {
     data: dataFollowers,
     loading: loadingFollowers,
@@ -26,20 +32,36 @@ export default function Follow({ username }) {
   }
   const { getFollowers } = dataFollowers;
 
+  const openFollowers = () => {
+    setShowModal(true);
+    setTitleModal("Seguidores");
+    setChildrenModal(
+      <ListUsers users={getFollowers} setShowModal={setShowModal} />
+    );
+  };
+
   return (
-    <div className="followers">
-      <p>
-        {" "}
-        <span>**</span> publicaciones
-      </p>
-      <p className="link">
-        {" "}
-        <span>{size(getFollowers)}</span> seguidores
-      </p>
-      <p className="link">
-        {" "}
-        <span>**</span> seguidos
-      </p>
-    </div>
+    <>
+      <div className="followers">
+        <p>
+          {" "}
+          <span>**</span> publicaciones
+        </p>
+        <p className="link" onClick={openFollowers}>
+          {" "}
+          <span>{size(getFollowers)}</span> seguidores
+        </p>
+        <p className="link">
+          {" "}
+          <span>**</span> seguidos
+        </p>
+      </div>
+      <ModalBasic
+        show={showModal}
+        setShow={setShowModal}
+        title={titleModal}
+        children={childrenModal}
+      ></ModalBasic>
+    </>
   );
 }
