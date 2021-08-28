d14eff61434a3dbaf16eb023f6b4441aeefa1e13
Charly_Angel <c.a.a.a.503@gmail.com>
2021-08-28 00:20:03 -0500

    fix: errores del linter

--- src/components/User/Profile/Profile.js
+++ src/components/User/Profile/Profile.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import { Grid, Image } from "semantic-ui-react";
-import { useQuery } from "@apollo/client";
-import { GET_USER } from "../../../gql/user";
-import userAuth from "../../../hooks/useAuth";
-import AvatarForm from "../AvatarForm";
-import UserNotFound from "../../UserNotFound";
-import ModalBasic from "../../Modal/ModalBasic";
-import ImageNotFound from "../../../assets/png/avatar.png";
-import HeaderProfile from "./HeaderProfile";
-import SettingsForm from "../SettingsForm";
-import Followers from "./Followers";
-import "./Profile.scss";
+import React, { useState } from 'react';
+import { Grid, Image } from 'semantic-ui-react';
+import { useQuery } from '@apollo/client';
+import { GET_USER } from '../../../gql/user';
+import userAuth from '../../../hooks/useAuth';
+import AvatarForm from '../AvatarForm';
+import UserNotFound from '../../UserNotFound';
+import ModalBasic from '../../Modal/ModalBasic';
+import ImageNotFound from '../../../assets/png/avatar.png';
+import HeaderProfile from './HeaderProfile';
+import SettingsForm from '../SettingsForm';
+import Followers from './Followers';
+import './Profile.scss';
 
 export default function Profile({ username, totalPublications }) {
   const [showModal, setShowModal] = useState(false);
-  const [titleModal, setTitleModal] = useState("");
+  const [titleModal, setTitleModal] = useState('');
   const [childrenModal, setChildrenModal] = useState(false);
   const { auth } = userAuth();
 
@@ -31,15 +31,15 @@ export default function Profile({ username, totalPublications }) {
 
   const handleModal = (typeModal) => {
     switch (typeModal) {
-      case "avatar":
-        setTitleModal("Cambiar foto de perfil");
+      case 'avatar':
+        setTitleModal('Cambiar foto de perfil');
         setChildrenModal(
-          <AvatarForm setShowModal={setShowModal} auth={auth} />
+          <AvatarForm setShowModal={setShowModal} auth={auth} />,
         );
         setShowModal(true);
         break;
-      case "settings":
-        setTitleModal("Ajustes");
+      case 'settings':
+        setTitleModal('Ajustes');
         setChildrenModal(
           <SettingsForm
             setShowModal={setShowModal}
@@ -47,7 +47,7 @@ export default function Profile({ username, totalPublications }) {
             setChildrenModal={setChildrenModal}
             getUser={getUser}
             refetch={refetch}
-          />
+          />,
         );
         setShowModal(true);
         break;
@@ -58,15 +58,15 @@ export default function Profile({ username, totalPublications }) {
 
   return (
     <>
-      <Grid className="profile">
-        <Grid.Column width={5} className="profile__left">
+      <Grid className='profile'>
+        <Grid.Column width={5} className='profile__left'>
           <Image
             src={getUser.avatar ? getUser.avatar : ImageNotFound}
             avatar
-            onClick={() => username === auth.username && handleModal("avatar")}
+            onClick={() => username === auth.username && handleModal('avatar')}
           />
         </Grid.Column>
-        <Grid.Column width={11} className="profile__right">
+        <Grid.Column width={11} className='profile__right'>
           <HeaderProfile
             username={username}
             auth={auth}
@@ -76,16 +76,21 @@ export default function Profile({ username, totalPublications }) {
             username={username}
             totalPublications={totalPublications}
           />
-          <div className="other">
-            <p className="name">{getUser.name}</p>
+          <div className='other'>
+            <p className='name'>{getUser.name}</p>
             {getUser.siteWeb && (
-              <a href={getUser.siteWeb} className="siteWeb" target="_blank">
+              <a
+                href={getUser.siteWeb}
+                className='siteWeb'
+                target='_blank'
+                rel='noreferrer'
+              >
                 {getUser.siteWeb}
               </a>
             )}
 
             {getUser.description && (
-              <p className="description">{getUser.description}</p>
+              <p className='description'>{getUser.description}</p>
             )}
           </div>
         </Grid.Column>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,16 @@
-import React from "react";
-import { Grid } from "semantic-ui-react";
-import Feed from "../../components/Home/Feed";
-import UsersNotFolloweds from "../../components/Home/UsersNotFolloweds";
-import useAuth from "../../hooks/useAuth";
-import "./Home.scss";
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import Feed from '../../components/Home/Feed';
+import UsersNotFolloweds from '../../components/Home/UsersNotFolloweds';
+import './Home.scss';
 
 export default function Home() {
-  const auth = useAuth();
   return (
-    <Grid className="home">
-      <Grid.Column className="home__left" width={11}>
+    <Grid className='home'>
+      <Grid.Column className='home__left' width={11}>
         <Feed />
       </Grid.Column>
-      <Grid.Column className="home__right" width={5}>
+      <Grid.Column className='home__right' width={5}>
         <UsersNotFolloweds />
       </Grid.Column>
     </Grid>
